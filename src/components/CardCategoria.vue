a06f148ade223f5d6e505397de4aa124d59e5c4c
claudeny.avelino <claudeny.avelino@hotmail.com>
2026-05-23 15:39:50 -0300

    Reutilizando CSS com componentes

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -1,12 +1,14 @@
 <script lang="ts">
 import type ICategoria from './interface/ICategoria';
 import type { PropType } from 'vue';
+import Tag from './Tag.vue';
 
 
 export default {
     props: {
         categoria: { type: Object as PropType<ICategoria>, required: true }
-    }
+    },
+    components: { Tag }
 }
 </script>
 
@@ -20,7 +22,7 @@ export default {
 
         <ul class="categoria__ingredientes">
             <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
-                {{ ingrediente }}
+                <Tag :texto="ingrediente" />
             </li>
         </ul>
     </article>
